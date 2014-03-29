4bd900e646adc2544bb19a58bb5207d24019752d
Alex Fedorko <alex@fedorko.me>
2014-03-30 03:39:08 +0400

    fix default postgresql encoding

--- modules/postgresql/manifests/init.pp
+++ modules/postgresql/manifests/init.pp
@@ -21,7 +21,15 @@ class postgresql
         "postgresql-client":
         ensure  => present
     }
-
+    
+    # http://stackoverflow.com/questions/15949783/postgresql-is-being-installed-with-sql-ascii-using-puppet
+    exec { 'psql_utf8_fix':
+        require => Package["postgresql-9.3"],
+        command => 'pg_dropcluster --stop 9.3 main ; pg_createcluster --start --locale en_US.UTF-8 9.3 main',
+        unless  => 'sudo -u postgres psql -t -c "\l" | grep template1 | grep -q UTF',
+        path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    }
+    
     service 
     { 
         "postgresql":
